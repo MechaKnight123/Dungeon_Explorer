628a3939b35805ffed4f4afec1668eb8c530f6e9
MechaKnight123 <mechajoel123@gmail.com>
2025-03-08 03:32:37 +0000

    Deleted code from the Game method(should contain object only) and in the Start method, code is written to ask the user if they want to play the game

--- Game.cs
+++ Game.cs
@@ -12,21 +12,19 @@ namespace DungeonExplorer
         public Game()
         {
             // Initialize the game with one room and one player
-            Player player = new Player();
-            Room currentRoom = new Room();
-
-            Console.WriteLine("Please enter your name: ");
-            string name=Console.ReadLine();
-            player.Players(name);
-
-            Console.WriteLine("Please enter the Room name to start the game (bedroom/bathroom/dining room): ");
-            string Roomname = Console.ReadLine();
-            string description = currentRoom.GetDescription(Roomname);
+            
         }
         public void Start()
         {
             // Change the playing logic into true and populate the while loop
             bool playing = false;
+            Console.WriteLine("Are you ready to start? Enter y/n");
+            string input = Console.ReadLine();
+            if (!string.Equals("y", input, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Goodbye!");
+                return;
+            }
             while (playing)
             {
                 // Code your playing logic here
