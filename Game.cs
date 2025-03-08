2fda62bf50b4e3e5c2b8559bf0a28c116ac51e25
MechaKnight123 <mechajoel123@gmail.com>
2025-03-08 05:00:09 +0000

    While the user plays the game, the user is prompted to enter either 1,2 or 3 ; and each number will perform a different task, as explained by the comments in the code. The PickUpItem method created ensures that the user doesn't pick up an item that already exists in their inventory.

--- Game.cs
+++ Game.cs
@@ -32,8 +32,49 @@ namespace DungeonExplorer
 
             while (playing)
             {
-                // Code your playing logic here
+                //Asks user to enter 1, 2 or 3
+                string initialIntro = "Enter 1 to add an item, 2. to view room's description and 3. to get current items and health.";
+               
+                //An intro message to the game is displayed and users input is store in the variable nextStep 
+                Console.WriteLine($"Hey {playersName}. Welcome to the dungeons! \n{initialIntro}");
+                string nextStep = Console.ReadLine();
+                
+                //The value entered by the user will determine the next steps the program takes
+                if (string.Equals("1", nextStep, StringComparison.OrdinalIgnoreCase))
+                {
+                    PickUpItem(); // Adds item to inventory when 1 is inputted
+                }
+                else if (string.Equals("2", nextStep, StringComparison.OrdinalIgnoreCase))
+                {   
+                    // Displays rooms decription if user enters 2
+                    Console.WriteLine($"Current room's description is: {currentRoom.GetDescription()}");
+                }
+                else if (string.Equals("3", nextStep, StringComparison.OrdinalIgnoreCase))
+                {
+                    //Displays inventory and health when user enters 3
+                    Console.WriteLine($"You have these items: {player.InventoryContents()}. \nYour health is: {player.Health}");
+                }
+                else
+                {   
+                    // Any other input other than 1,2 or 3 results in Invalid message
+                    Console.WriteLine($"Invalid input. \n {initialIntro}");
+                }
 
+            }
+        }
+        private void PickUpItem()
+        {
+            string item = currentRoom.GetItem();
+            bool ownItem = player.DoesItemExist(item);
+            if (ownItem)
+            {
+                Console.WriteLine("You already have this item");
+            }
+            else
+            {
+                //if ownItem is false- item doesnt exist in inventory, Item is successfully added to the inventory
+                player.PickUpItem(item);
+                Console.WriteLine($"You have successfully picked up item: {item}");
             }
         }
     }
