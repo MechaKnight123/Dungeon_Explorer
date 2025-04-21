15b3bd81bf2bbab7af9836683db4f4e94ce443ca
MechaKnight123 <mechajoel123@gmail.com>
2025-04-21 19:45:42 +0100

    IDamageable file which contains the IDamageable interface has been created
    This contains the Attack and TakeDamage void functions, currently empty in this new file, but need to be overrided in player and monster as they will attack and take damage differently. Creature file will inherit IDamageable.

---
+++ IDamageable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+	internal interface IDamageable
+	{
+        void TakeDamage();
+        void Attack();
+		
+		
+	}
+}
