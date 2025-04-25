0a3d9f55fa1b0e7604fd843d1b99ab6ddcbf92bd
MechaKnight123 <mechajoel123@gmail.com>
2025-04-25 13:14:51 +0100

    Creating a separate file for each Monster, deep inheritance

---
+++ Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    class Dog : Monster
+    {
+        public Dog(string name, int health) : base(name, health) { }
+    }
+}
---
+++ Tower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    class Tower : Monster
+    {
+        public Tower(string name, int health) : base(name, health) { }
+    }
+}
