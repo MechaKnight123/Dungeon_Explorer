f110345f1b2b35480d6c5fc36090cf56d7cf2e51
MechaKnight123 <mechajoel123@gmail.com>
2025-04-27 01:22:41 +0100

    Item class inherits from ICollectible and Potion and Weapon class inherits from Item class. ICollectible has 2 void functions: OnCollect and Use which will be overriden in each Potion and weapon as each one has a different functionality/ purpose.

--- ICollectible.cs
+++ ICollectible.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace DungeonExplorer
 {
 	public interface ICollectible
 	{
-		
+		//Void functions that will be used by items
+		void OnCollect(Player player);
+		void Use(Player player);
+
 	}
 
 }
--- Item.cs
+++ Item.cs
@@ -5,18 +5,26 @@ using System.Collections.Generic;
 namespace DungeonExplorer
 {
   
-    public abstract class Item
+    public abstract class Item: ICollectible
 	{
         public string itemName { get; protected set; }
-        public int itemDamage { get; protected set; }
+        public int itemAmount { get; protected set; }
 
         //Constructor
-        public Item(string itemname, int itemdamage)
+        public Item(string itemname, int itemamount)
         {
             this.itemName = itemname;
-            this.itemDamage = itemdamage;
+            this.itemAmount = itemamount;
         }
 
+        //This is from the ICollectible interface and these void functions will be overrided in each potion and weapon separately as each has a unique function
+        public virtual void OnCollect(Player player)
+        {
+            Console.WriteLine($"{player.Name} picked up {itemName}");
+            player.PickUpItem(itemName);
+        }
+
+        public virtual void Use(Player player);
 
     }
 }
