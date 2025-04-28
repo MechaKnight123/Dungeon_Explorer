dfd47d5c3db4d24423b04c4fed0c8feb10e43000
MechaKnight123 <mechajoel123@gmail.com>
2025-04-28 15:30:20 +0100

    Created a file for each item
    Files for each item has been created and instantiated. Each item has its own purpose/use and will deal damage to the Monster differently. The void functions OnCollect and Use have been modified according to the item being used.

---
+++ FireballPotion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    public class FireballPotion : Potion
+    {
+        //initialise Healing Potion by using inherited values of itemname and itemdamage
+        public FireballPotion(string itemname, int itemamount) : base(itemname, itemamount) { }
+
+        //Writes in the console that the user has picked up this item, and actually adds to inventory
+        public virtual void OnCollect(Player player)
+        {
+            Console.WriteLine($"{player.Name} picked up {itemName}");
+            player.PickUpItem(itemName);
+        }
+
+        //Explains the effect of using item
+        public virtual void Use(Player player, Monster monster)
+        {
+            Console.WriteLine($"{player.Name} chucks the {itemName} and causes {itemAmount} damage!");
+            player.Attack(monster, itemAmount)
+        }
+
+    }
+}
---
+++ HealingPotion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    public class HealingPotion : Potion
+    {
+        //initialise Healing Potion by using inherited values of itemname and itemdamage
+        public HealingPotion(string itemname, int itemamount) : base(itemname, itemamount) { }
+
+        //Writes in the console that the user has picked up this item, and actually adds to inventory
+        public virtual void OnCollect(Player player)
+        {
+            Console.WriteLine($"{player.Name} picked up {itemName}");
+            player.PickUpItem(itemName);
+        }
+
+        //Explains the effect of using item
+        public virtual void Use(Player player, Monster monster)
+        {
+            Console.WriteLine($"{player.Name} heals by using the {itemName} by {itemAmount} ");
+            player.Health+= itemAmount
+        }
+
+    }
+}
