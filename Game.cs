7af236c0a02c308fc5d5794901458348f75abcda
MechaKnight123 <mechajoel123@gmail.com>
2025-04-29 18:20:21 +0100

    Created a GameMap class that will allow the user to navigate through the dungeon. I have also created a SetupRooms function in this class and in it I have taken everything that used to be in the Game class and put it into the GameMap class. I then needed to modify PickupItem private void function because a lot of objects were innaccessible and I used the IteminRoom object and functions in the item class to resolve issues

--- Game.cs
+++ Game.cs
@@ -7,42 +7,12 @@ namespace DungeonExplorer
     internal class Game
     {
         private Player player;
-        private Room currentRoom;
-        private Room Room1, Room2, Room3, Room4, Room5,Room6,Room7;
+        private GameMap gameMap;
         
 
         public Game()
         {
-            // Initialize the game with 5 rooms with a description, item, name,key and a monster- there is a current room to indicate the room the user is in
-            //User starts the Game in Precious patio room
-            currentRoom = new Room("This is the Precious Patio where unique things happen", "Welcome Mat", "Precious Patio","Room1 key");
-            Room1 = new Room("This is the Precious Patio where unique things happen", "Welcome Mat", "Precious Patio", "Room1 key");
-            Room2 = new Room("This is the Dragon's dungeon where there have been many fallen soldiers", "Diamond Sword", "Dragon's dungeon", "Room2 key");
-            Room3 = new Room("This is the Snappy Crocodile's swamp where things happen unexpectedly quick", "Poison Potion", "Snappy swamp", "Room3 key");
-            Room4 = new Room("This is the Dark Knight's domain and not many who visit this place are able to tell the tale", "Fireball potion", "Dark Knight's potion", "Room4 key");
-            Room5 = new Room("This is Krusty Krab's lagoon where there is a strange and mysterious creature", "Healing potion", "Krusty Krab's Lagoon", "Room5 key");
-            Room6 = new Room("This is Pleasant Park where there is an ominous and ferocious dog", "Leafblower", "Pleasant Park", "Room6 key");
-            Room7 = new Room("This is Tilted Towers where the buildings are all shaped peculiarly", "Canon", "Tilted Towers", "Room7 key");
-
-            //Every room has a Monster with a unique name and health and even effects
-            currentRoom.Occupant = new Porcupine("Petite porcupine", 25);
-            Room1.Occupant = new Porcupine("Petite porcupine",25);
-            Room2.Occupant = new Dragon("Dangerous Dragon",30);
-            Room3.Occupant = new Crocodile("Snappy the Crocodile",35);
-            Room4.Occupant = new Knight("Dark Knight",40);
-            Room5.Occupant = new Crab("Krusty Krab",45);
-            Room6.Occupant = new Dog("Ferocious Dog",50);
-            Room7.Occupant = new Tower("Tiger Tower",55);
-
-            //Every room has a Item with itemName and itemAmount and will either be a Potion or Weapon
-            currentRoom.ItemInRoom = new WelcomeMat("Welcome Mat", 30);
-            Room1.ItemInRoom = new WelcomeMat("Welcome Mat", 30);
-            Room2.ItemInRoom = new DiamondSword("Diamond Sword", 50);
-            Room3.ItemInRoom = new PoisonPotion("Poison Potion", 35);
-            Room4.ItemInRoom = new FireballPotion("Fireball potion", 40);
-            Room5.ItemInRoom = new HealingPotion("Healing potion", 100);
-            Room6.ItemInRoom = new LeafBlower("Leafblower", 10);
-            Room7.ItemInRoom = new Canon("Canon", 55);
+            gameMap = new GameMap();
 
         }
         public void Start()
@@ -62,6 +32,7 @@ namespace DungeonExplorer
             player = new Player(playersName,100); // Player object has been created with name inputted by user and health set to 100
             while (playing)
             {
+                Room currentRoom = gameMap.CurrentRoom;
                 //Asks user to enter 1, 2, 3 or 4
                 string initialIntro = "Enter 1 to add an item, 2. to view room's description, 3. to get current items and health and 4. to exit the program";
                
@@ -103,8 +74,9 @@ namespace DungeonExplorer
         }
         private void PickUpItem()
         {
-            string item = currentRoom.GetItem();
-            bool ownItem = player.DoesItemExist(item);
+            Room room = gameMap.CurrentRoom;
+            string itemName = room.ItemInRoom.itemName;
+            bool ownItem = player.DoesItemExist(itemName);
             if (ownItem)
             {
                 Console.WriteLine("You already have this item");
@@ -112,8 +84,10 @@ namespace DungeonExplorer
             else
             {
                 //if ownItem is false- item doesnt exist in inventory, Item is successfully added to the inventory
-                player.PickUpItem(item);
-                Console.WriteLine($"You have successfully picked up item: {item}");
+                room.ItemInRoom.OnCollect(player); // Adds item via the item class
+                Console.WriteLine($"You have successfully picked up item: {itemName}");
+                room.ItemInRoom = null; // Remove item from room after pickup
+               
             }
         }
     }
--- GameMap.cs
+++ GameMap.cs
@@ -8,9 +8,48 @@ namespace DungeonExplorer
 {
 	public class GameMap
 	{
-		public GameMap()
+        
+        private Room Room1, Room2, Room3, Room4, Room5, Room6, Room7;
+        private List<Room> rooms = new List<Room>();
+        public Room CurrentRoom { get; private set; }
+
+        public GameMap()
 		{
-		}
-	}
+            SetupRooms();
+        }
+		private void SetupRooms()
+		{
+
+            // Initialize the game with 5 rooms with a description, item, name,key and a monster- there is a current room to indicate the room the user is in
+            //User starts the Game in Precious patio room    
+            Room1 = new Room("This is the Precious Patio where unique things happen", "Welcome Mat", "Precious Patio", "Room1 key");
+            Room2 = new Room("This is the Dragon's dungeon where there have been many fallen soldiers", "Diamond Sword", "Dragon's dungeon", "Room2 key");
+            Room3 = new Room("This is the Snappy Crocodile's swamp where things happen unexpectedly quick", "Poison Potion", "Snappy swamp", "Room3 key");
+            Room4 = new Room("This is the Dark Knight's domain and not many who visit this place are able to tell the tale", "Fireball potion", "Dark Knight's potion", "Room4 key");
+            Room5 = new Room("This is Krusty Krab's lagoon where there is a strange and mysterious creature", "Healing potion", "Krusty Krab's Lagoon", "Room5 key");
+            Room6 = new Room("This is Pleasant Park where there is an ominous and ferocious dog", "Leafblower", "Pleasant Park", "Room6 key");
+            Room7 = new Room("This is Tilted Towers where the buildings are all shaped peculiarly", "Canon", "Tilted Towers", "Room7 key");
+
+            //Every room has a Monster with a unique name and health and even effects
+            Room1.Occupant = new Porcupine("Petite porcupine", 25);
+            Room2.Occupant = new Dragon("Dangerous Dragon", 30);
+            Room3.Occupant = new Crocodile("Snappy the Crocodile", 35);
+            Room4.Occupant = new Knight("Dark Knight", 40);
+            Room5.Occupant = new Crab("Krusty Krab", 45);
+            Room6.Occupant = new Dog("Ferocious Dog", 50);
+            Room7.Occupant = new Tower("Tiger Tower", 55);
+
+            //Every room has a Item with itemName and itemAmount and will either be a Potion or Weapon
+            Room1.ItemInRoom = new WelcomeMat("Welcome Mat", 30);
+            Room2.ItemInRoom = new DiamondSword("Diamond Sword", 50);
+            Room3.ItemInRoom = new PoisonPotion("Poison Potion", 35);
+            Room4.ItemInRoom = new FireballPotion("Fireball potion", 40);
+            Room5.ItemInRoom = new HealingPotion("Healing potion", 100);
+            Room6.ItemInRoom = new LeafBlower("Leafblower", 10);
+            Room7.ItemInRoom = new Canon("Canon", 55);
+
+            CurrentRoom = Room1;
+        }
+    }
 
 }
