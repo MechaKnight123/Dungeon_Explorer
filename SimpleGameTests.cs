a483b49a983e69790c622e2a87e11de6ce68bb1c
MechaKnight123 <mechajoel123@gmail.com>
2025-05-01 06:29:20 +0100

    Created void function RunAll and Test_PlayerCanPickUpItem.  RunAll function will be used in program file to run all tests and ensure everything works as intended whereas the other void function is used to ensure that the user can pick up an item

--- SimpleGameTests.cs
+++ SimpleGameTests.cs
@@ -1,10 +1,39 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DungeonExplorer;
+using System;
 
-namespace DungeonExplorerTests
+namespace DungeonExplorer
 {
+    public static class ManualTests
+    {
+        //will be used in program file to run all tests and ensure everything works as intended
+        public static void RunAll()
+        {
+            Console.WriteLine("Running manual tests...\n");
+
+            //tests to be run should be run below
+            Test_PlayerCanPickUpItem();
+
+            Console.WriteLine("\n All manual tests completed.");
+        }
+
+        //used to check if the player can pick up an item - just used Welcome Mat as an example
+        private static void Test_PlayerCanPickUpItem()
+        {
+            var player = new Player("TestPlayer", 100);
+            var item = new WelcomeMat("Welcome Mat", 10);
+            item.OnCollect(player);
+            
+            Console.WriteLine(player.DoesItemExist("Welcome Mat")//if true, a message saying that this function has passed will be displayed
+                ? "Test_PlayerCanPickUpItem passed."
+                : "Test_PlayerCanPickUpItem failed.");
+        }
 
 
+
+
+
+
+
+    }
 
 
 }
